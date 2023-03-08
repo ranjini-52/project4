fcc48abe4c2ffd82b61ab6e1601fda6fd2dd9f01
Your Nameranjini <ranjini782000@gmail.com>
2023-03-08 14:20:48 +0530

    first commit

---
+++ app4/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your tests here.
+def app4(request):
+    return HttpResponse('Iam Ranjini')
+def app5(request):
+    return HttpResponse('hello world')
+    
